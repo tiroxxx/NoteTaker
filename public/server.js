e3f3698350db34a09149a401a7368cabc6dce4ce
tiroxxx <christianmarquez41@gmail.com>
2020-10-08 20:29:19 -0400

    setting up store file where class of notes is defined

--- public/server.js
+++ public/server.js
@@ -11,6 +11,6 @@ app.use(express.urlencoded({ extended: true}))
 app.use(express.static("public"));
 
 // require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("../routes/htmlRoutes")(app);
 
 app.listen(PORT, () => console.log(`Listen on PORT: http://localhost:${PORT}`));
