8dca11389e5b5e40439358f613d34d149aa42424
tiroxxx <christianmarquez41@gmail.com>
2020-10-08 20:00:52 -0400

    added everything in the start up video

---
+++ public/server.js
@@ -0,0 +1,16 @@
+// require Express
+const express = require("express");
+
+// initialize app and create port
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+
+app.use(express.static("public"));
+
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => console.log(`Listen on PORT: http://localhost:${PORT}`));
