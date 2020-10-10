7a0c8092a66107040b8317ad8db336bcaaed91a9
tiroxxx <christianmarquez41@gmail.com>
2020-10-09 20:13:07 -0400

    got rid of the class notes because I couldn't figure out how to use a class to implement the functionality, using a array instead

--- db/store.js
+++ db/store.js
@@ -1,28 +1,4 @@
-const fs = require("fs");
+// note storage
+let notes = [{}];
 
-// class for your notes
-class Store {
-    constructor(title, notes){
-        this.title = title;
-        this.notes = notes;
-    }
-
-    readNote(){
-        return fs.readFile(__dirname + "/db.json", function(err){
-            if(err) throw err;
-        })
-    }
-
-    writeNote(){
-        return fs.writeFile(__dirname + "/db.json", this.notes, function(err){
-            if(err) throw err;
-        })
-    }
-
-    deleteNote(){
-
-    }
-}
-
-module.exports = Store;
-// defined fuctions that read, write, and delete
+module.exports = noteContents;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,26 @@
-const dbJson = require("../db/db.json");
-const store = require("../db/store");
+const notes = require("../db/store");
+const fs = require("fs");
 
 
-module.exports = function(app) {
-    app.get("/api/notes", function(req, res) {
+module.exports = function (app) {
+    // Get route
+    app.get("/api/notes", function (req, res) {
+        res.json(notes);
+    });
+    // post route
+    app.post("/api/notes", function(req,res) {
+        console.log(req.body);
+        // catching the new note
+        const newNote = req.body;
+        // giving the new note the next available ID
+        newNote.id = notes[notes.length - 1].id + 1;
+        console.log(newNote.id);
+        // adding the new note to my storage
         
-    })
+
+    });
+
+    // delete
+
+
 }
-// Get route
-
-// post route
-
-// delete
